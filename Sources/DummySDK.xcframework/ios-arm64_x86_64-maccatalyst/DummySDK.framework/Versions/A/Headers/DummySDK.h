d17c005f66e5988f587c049a71772d0d33b12f2c
DHEERAJ <dheeraj@appscrip.co>
2022-10-31 14:06:17 +0530

    test

---
+++ Sources/DummySDK.xcframework/ios-arm64_x86_64-maccatalyst/DummySDK.framework/Versions/A/Headers/DummySDK.h
@@ -0,0 +1,18 @@
+//
+//  DummySDK.h
+//  DummySDK
+//
+//  Created by Dheeraj Kumar Sharma on 31/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummySDK.
+FOUNDATION_EXPORT double DummySDKVersionNumber;
+
+//! Project version string for DummySDK.
+FOUNDATION_EXPORT const unsigned char DummySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummySDK/PublicHeader.h>
+
+
--- Sources/DummySDK/DummySDK.swift
+++
@@ -1,6 +0,0 @@
-public struct DummySDK {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
